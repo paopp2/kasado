d6ec44b21251d453ecdac854c7d804ccda167d31
Paolo Pepito <npaolopepito@gmail.com>
2022-02-15 11:49:01 +0800

    Add base Firebase dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:kasado/firebase_options.dart';
 import 'package:kasado/ui/auth/login_view.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
