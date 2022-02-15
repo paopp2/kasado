4918cf2ddbe387efd18cbf9f5b30086ecceefe42
Paolo Pepito <npaolopepito@gmail.com>
2022-02-15 13:28:14 +0800

    Add currentUserProvider as core providers

--- lib/data/core/core_providers.dart
+++ lib/data/core/core_providers.dart
@@ -1,7 +1,21 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kasado/model/kasado_user/kasado_user.dart';
 
 final fireauthProvider = Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
 
 final authStateChangesProvider = StreamProvider<User?>(
     (ref) => ref.watch(fireauthProvider).authStateChanges());
+
+final currentUserProvider = Provider.autoDispose<KasadoUser?>((ref) {
+  User? firebaseUser = ref.watch(fireauthProvider).currentUser;
+
+  if (firebaseUser == null) return null;
+  return KasadoUser(
+    id: firebaseUser.uid,
+    displayName: firebaseUser.displayName,
+    email: firebaseUser.email,
+    phoneNumber: firebaseUser.phoneNumber,
+    photoUrl: firebaseUser.photoURL,
+  );
+});
