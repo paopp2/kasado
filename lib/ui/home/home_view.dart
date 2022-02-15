af7add0ebb0dbf5b78dca99d4b926f96fc789584
Paolo Pepito <npaolopepito@gmail.com>
2022-02-15 19:10:57 +0800

    Add temp design for HomeView

--- lib/ui/home/home_view.dart
+++ lib/ui/home/home_view.dart
@@ -9,18 +9,183 @@ class HomeView extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final model = ref.watch(homeViewModel);
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-              onPressed: model.signOut,
-              child: const Text('Sign out'),
-            )
-          ],
-        ),
-      ),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {},
+            ),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.logout),
+                onPressed: model.signOut,
+              )
+            ],
+          ),
+          body: Center(
+            child: ListView(
+              children: [
+                const SizedBox(height: 50),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 25,
+                    vertical: 10,
+                  ),
+                  child: SizedBox(
+                    height: constraints.maxHeight * 0.4,
+                    width: constraints.maxWidth * 0.85,
+                    child: Card(
+                      elevation: 20,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Center(
+                          child: Column(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(30),
+                                child: SizedBox(
+                                  height: constraints.maxHeight * 0.2,
+                                  width: constraints.maxWidth,
+                                  child: Image.network(
+                                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHonQRDPCLA0zXvTzoThs-Q2hQ9s5lg3RBmA&usqp=CAU",
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: constraints.maxHeight * 0.02),
+                              const Text(
+                                'SKINA JAPAN BASKETBALL COURT',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              const Divider(),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 50.0,
+                                ),
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        const Icon(Icons.people),
+                                        SizedBox(
+                                            width: constraints.maxWidth * 0.05),
+                                        const Text('8 / 15')
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        const Icon(Icons.calendar_today),
+                                        SizedBox(
+                                          width: constraints.maxWidth * 0.05,
+                                        ),
+                                        const Text('Feb 15 / 3:00 - 4:00')
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        const Icon(Icons.money),
+                                        SizedBox(
+                                          width: constraints.maxWidth * 0.05,
+                                        ),
+                                        const Text('₱ 50')
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 25,
+                    vertical: 10,
+                  ),
+                  child: SizedBox(
+                    height: constraints.maxHeight * 0.4,
+                    width: constraints.maxWidth * 0.85,
+                    child: Card(
+                      elevation: 20,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Center(
+                          child: Column(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(30),
+                                child: SizedBox(
+                                  height: constraints.maxHeight * 0.2,
+                                  width: constraints.maxWidth,
+                                  child: Image.network(
+                                    "https://i.pinimg.com/originals/80/9f/af/809faf105f55a2830918b9d859bd3958.jpg",
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: constraints.maxHeight * 0.02),
+                              const Text(
+                                'SKINA CHINA BASKETBALL COURT',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              const Divider(),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 50.0,
+                                ),
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        const Icon(Icons.people),
+                                        SizedBox(
+                                            width: constraints.maxWidth * 0.05),
+                                        const Text('12 / 15')
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        const Icon(Icons.calendar_today),
+                                        SizedBox(
+                                          width: constraints.maxWidth * 0.05,
+                                        ),
+                                        const Text('Feb 15 / 4:00 - 5:00')
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        const Icon(Icons.money),
+                                        SizedBox(
+                                          width: constraints.maxWidth * 0.05,
+                                        ),
+                                        const Text('₱ 20')
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
