e00db486970a8a59b5700f0baf84d4ac8c1ce0ab
Paolo Pepito <npaolopepito@gmail.com>
2022-02-16 18:42:27 +0800

    Implement initial design for CourtsOwnedView

--- lib/ui/courts_owned/courts_owned_view.dart
+++ lib/ui/courts_owned/courts_owned_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:kasado/app_router.dart';
+import 'package:kasado/model/court/court.dart';
 
 class CourtsOwnedView extends StatelessWidget {
   const CourtsOwnedView({Key? key}) : super(key: key);
@@ -8,7 +11,29 @@ class CourtsOwnedView extends StatelessWidget {
     return LayoutBuilder(
       builder: (context, constraints) {
         return Scaffold(
-          body: Container(color: Colors.blueGrey),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+          ),
+          body: ListView.builder(itemBuilder: (context, index) {
+            return ListTile(
+              title: Text('Pitogo Basketball Court $index'),
+              subtitle: const Text('Pitogo Consolacion Cebu, Philippines'),
+              onTap: () => context.pushNamed(
+                Routes.courtDetailsView,
+                extra: const Court(
+                  id: 'court2',
+                  name: 'Pitogo Basketball Court',
+                  photoUrl:
+                      "https://i.pinimg.com/originals/80/9f/af/809faf105f55a2830918b9d859bd3958.jpg",
+                  ticketPrice: 50,
+                ),
+              ),
+            );
+          }),
         );
       },
     );
