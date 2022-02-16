70e8aac86bc96d97b6b96ae8204e54fb305c37e6
Paolo Pepito <npaolopepito@gmail.com>
2022-02-16 12:23:25 +0800

    Initial design and implementation for Court Schedule

--- lib/ui/court_details/court_details_view.dart
+++ lib/ui/court_details/court_details_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class CourtDetailsView extends StatelessWidget {
   const CourtDetailsView({Key? key}) : super(key: key);
@@ -6,11 +8,132 @@ class CourtDetailsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
-      builder: (constraints, context) {
+      builder: (context, constraints) {
         return Scaffold(
-          body: Container(color: Colors.blue),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30),
+                ),
+                child: SizedBox(
+                  height: constraints.maxHeight * 0.3,
+                  child: Image.network(
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHonQRDPCLA0zXvTzoThs-Q2hQ9s5lg3RBmA&usqp=CAU",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Text('Skina Japan Basketball Court'.toUpperCase()),
+              const SizedBox(height: 30),
+              Expanded(
+                child: SfCalendar(
+                  view: CalendarView.schedule,
+                  dataSource: _getCalendarDataSource(),
+                  scheduleViewSettings: ScheduleViewSettings(
+                    appointmentItemHeight: constraints.maxHeight * 0.1,
+                  ),
+                  appointmentBuilder: (context, details) {
+                    return Column(
+                      children: details.appointments.map((a) {
+                        a as Appointment;
+                        return Container(
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Material(
+                            color: Colors.transparent,
+                            child: InkWell(
+                              onTap: () {},
+                              child: ListTile(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                textColor: Colors.white,
+                                title: const Text('Available'),
+                                subtitle: Text(
+                                    "${DateFormat('h:mm').format(a.startTime)} - ${DateFormat('h:mm').format(a.endTime)}"),
+                                trailing: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: const [
+                                    Text('Players'),
+                                    Text(
+                                      '8 / 15',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 25,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         );
       },
     );
+  }
+}
+
+_AppointmentDataSource _getCalendarDataSource() {
+  List<Appointment> appointments = <Appointment>[];
+  appointments.addAll([
+    Appointment(
+      startTime: DateTime(2022, 2, 16, 8, 0),
+      endTime: DateTime(2022, 2, 16, 9, 0),
+      subject: 'Players: 8 / 15',
+      color: Colors.green,
+    ),
+    Appointment(
+      startTime: DateTime(2022, 2, 16, 9, 0),
+      endTime: DateTime(2022, 2, 16, 10, 0),
+      subject: 'Players: 12 / 15',
+      color: Colors.green,
+    ),
+    Appointment(
+      startTime: DateTime(2022, 2, 16, 11, 0),
+      endTime: DateTime(2022, 2, 16, 12, 0),
+      subject: 'Players: 15 / 15',
+      color: Colors.red,
+    ),
+    Appointment(
+      startTime: DateTime(2022, 2, 17, 8, 0),
+      endTime: DateTime(2022, 2, 16, 9, 0),
+      subject: 'Players: 8 / 15',
+      color: Colors.green,
+    ),
+    Appointment(
+      startTime: DateTime(2022, 2, 17, 9, 0),
+      endTime: DateTime(2022, 2, 17, 10, 0),
+      subject: 'Players: 15 / 15',
+      color: Colors.red,
+    ),
+    Appointment(
+      startTime: DateTime(2022, 2, 17, 10, 0),
+      endTime: DateTime(2022, 2, 17, 11, 0),
+      subject: 'Players: 8 / 15',
+      color: Colors.green,
+    ),
+  ]);
+
+  return _AppointmentDataSource(appointments);
+}
+
+class _AppointmentDataSource extends CalendarDataSource {
+  _AppointmentDataSource(List<Appointment> source) {
+    appointments = source;
   }
 }
