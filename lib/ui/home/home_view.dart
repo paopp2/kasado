4da85eff8ade20ed1502e95b382f0786efc65bde
Paolo Pepito <npaolopepito@gmail.com>
2022-02-16 15:00:19 +0800

    Refactor our HomeTab from HomeView

--- lib/ui/home/home_view.dart
+++ lib/ui/home/home_view.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kasado/logic/home/home_view_model.dart';
-import 'package:kasado/model/court/court.dart';
-import 'package:kasado/model/court_slot/court_slot.dart';
-import 'package:kasado/ui/home/components/court_slot_card.dart';
 import 'package:kasado/ui/home/components/user_info_drawer.dart';
+import 'package:kasado/ui/home/tabs/home/home_tab.dart';
 
 class HomeView extends HookConsumerWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final tabIndex = useState(0);
+    final tabController = useTabController(initialLength: 3);
     final model = ref.watch(homeViewModel);
 
     return SafeArea(
@@ -25,43 +26,39 @@ class HomeView extends HookConsumerWidget {
               elevation: 0,
               backgroundColor: Colors.transparent,
             ),
-            body: Center(
-              child: ListView(
-                children: [
-                  CourtSlotCard(
-                    model: model,
-                    constraints: constraints,
-                    courtSlot: CourtSlot(
-                      court: const Court(
-                        id: 'court1',
-                        name: 'Skina Japan Basketball Court',
-                        photoUrl:
-                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHonQRDPCLA0zXvTzoThs-Q2hQ9s5lg3RBmA&usqp=CAU",
-                        ticketPrice: 50,
-                      ),
-                      players: [],
-                      startsAt: DateTime(2022, 2, 16, 9, 0),
-                      endsAt: DateTime(2022, 2, 16, 10, 0),
-                    ),
-                  ),
-                  CourtSlotCard(
-                    model: model,
-                    constraints: constraints,
-                    courtSlot: CourtSlot(
-                      court: const Court(
-                        id: 'court2',
-                        name: 'Skina China Basketball Court',
-                        photoUrl:
-                            "https://i.pinimg.com/originals/80/9f/af/809faf105f55a2830918b9d859bd3958.jpg",
-                        ticketPrice: 15,
-                      ),
-                      players: [],
-                      startsAt: DateTime(2022, 2, 16, 8, 0),
-                      endsAt: DateTime(2022, 2, 16, 9, 0),
-                    ),
-                  ),
-                ],
-              ),
+            body: TabBarView(
+              physics: const NeverScrollableScrollPhysics(),
+              controller: tabController,
+              children: [
+                HomeTab(
+                  model: model,
+                  constraints: constraints,
+                ),
+                Container(color: Colors.amber),
+                Container(color: Colors.blue),
+              ],
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              onTap: (index) {
+                tabIndex.value = index;
+                tabController.animateTo(index);
+              },
+              currentIndex: tabController.index,
+              selectedItemColor: Colors.black,
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.money),
+                  label: 'Tickets',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person),
+                  label: 'Profile',
+                ),
+              ],
             ),
           );
         },
---
+++ lib/ui/home/tabs/home/home_tab.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:kasado/logic/home/home_view_model.dart';
+import 'package:kasado/model/court/court.dart';
+import 'package:kasado/model/court_slot/court_slot.dart';
+import 'package:kasado/ui/home/tabs/home/components/court_slot_card.dart';
+
+class HomeTab extends StatelessWidget {
+  const HomeTab({
+    Key? key,
+    required this.model,
+    required this.constraints,
+  }) : super(key: key);
+
+  final HomeViewModel model;
+  final BoxConstraints constraints;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ListView(
+        children: [
+          CourtSlotCard(
+            model: model,
+            constraints: constraints,
+            courtSlot: CourtSlot(
+              court: const Court(
+                id: 'court1',
+                name: 'Skina Japan Basketball Court',
+                photoUrl:
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHonQRDPCLA0zXvTzoThs-Q2hQ9s5lg3RBmA&usqp=CAU",
+                ticketPrice: 50,
+              ),
+              players: [],
+              startsAt: DateTime(2022, 2, 16, 9, 0),
+              endsAt: DateTime(2022, 2, 16, 10, 0),
+            ),
+          ),
+          CourtSlotCard(
+            model: model,
+            constraints: constraints,
+            courtSlot: CourtSlot(
+              court: const Court(
+                id: 'court2',
+                name: 'Skina China Basketball Court',
+                photoUrl:
+                    "https://i.pinimg.com/originals/80/9f/af/809faf105f55a2830918b9d859bd3958.jpg",
+                ticketPrice: 15,
+              ),
+              players: [],
+              startsAt: DateTime(2022, 2, 16, 8, 0),
+              endsAt: DateTime(2022, 2, 16, 9, 0),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
