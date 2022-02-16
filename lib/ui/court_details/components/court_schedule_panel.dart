4425dfb05361963a400a91473ea801c405b8f29b
Paolo Pepito <npaolopepito@gmail.com>
2022-02-16 19:54:26 +0800

    Refactor out CourtSchedulePanel from CourtDetailsView

---
+++ lib/ui/court_details/components/court_schedule_panel.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:kasado/ui/court_details/components/court_slot_details_dialog.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+class CourtSchedulePanel extends StatelessWidget {
+  const CourtSchedulePanel({
+    Key? key,
+    required this.isAdmin,
+    required this.constraints,
+  }) : super(key: key);
+
+  final bool isAdmin;
+  final BoxConstraints constraints;
+
+  @override
+  Widget build(BuildContext context) {
+    return SfCalendar(
+      view: CalendarView.schedule,
+      dataSource: _getCalendarDataSource(),
+      scheduleViewSettings: ScheduleViewSettings(
+        appointmentItemHeight: constraints.maxHeight * 0.1,
+      ),
+      appointmentBuilder: (context, details) {
+        return Column(
+          children: details.appointments.map((a) {
+            a as Appointment;
+            final isDone = a.startTime.isBefore(DateTime.now());
+            return Container(
+              decoration: BoxDecoration(
+                color: (isDone) ? Colors.grey.shade400 : Colors.green,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return CourtSlotDetailsDialog(
+                          constraints: constraints,
+                          isAdmin: isAdmin,
+                        );
+                      },
+                    );
+                  },
+                  child: ListTile(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    textColor: Colors.white,
+                    title: const Text('Available'),
+                    subtitle: Text(
+                        "${DateFormat('h:mm').format(a.startTime)} - ${DateFormat('h:mm a').format(a.endTime)}"),
+                    trailing: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        Text('Players'),
+                        Text(
+                          '0 / 15',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+}
+
+_AppointmentDataSource _getCalendarDataSource() {
+  List<Appointment> appointments = <Appointment>[];
+  appointments.addAll([
+    Appointment(
+      startTime: DateTime(2015, 1, 1, 7, 0),
+      endTime: DateTime(2015, 1, 1, 8, 0),
+      recurrenceRule: SfCalendar.generateRRule(
+        RecurrenceProperties(
+          recurrenceType: RecurrenceType.daily,
+          startDate: DateTime.now(),
+        ),
+        DateTime(2015, 1, 1, 7, 0),
+        DateTime(2015, 1, 1, 8, 0),
+      ),
+    ),
+    Appointment(
+      startTime: DateTime(2015, 1, 1, 14, 0),
+      endTime: DateTime(2015, 1, 1, 15, 0),
+      subject: 'Players: 0 / 15',
+      recurrenceRule: SfCalendar.generateRRule(
+        RecurrenceProperties(
+          recurrenceType: RecurrenceType.daily,
+          startDate: DateTime.now(),
+        ),
+        DateTime(2015, 1, 1, 14, 0),
+        DateTime(2015, 1, 1, 15, 0),
+      ),
+    ),
+  ]);
+
+  return _AppointmentDataSource(appointments);
+}
+
+class _AppointmentDataSource extends CalendarDataSource {
+  _AppointmentDataSource(List<Appointment> source) {
+    appointments = source;
+  }
+}
--- lib/ui/court_details/court_details_view.dart
+++ lib/ui/court_details/court_details_view.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kasado/model/court/court.dart';
-import 'package:kasado/ui/court_details/components/court_slot_details_dialog.dart';
-import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:kasado/ui/court_details/components/court_schedule_panel.dart';
 
-class CourtDetailsView extends StatelessWidget {
+class CourtDetailsView extends HookConsumerWidget {
   const CourtDetailsView({
     Key? key,
     required this.court,
@@ -15,7 +15,10 @@ class CourtDetailsView extends StatelessWidget {
   final bool isAdmin;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final tabIndex = useState(0);
+    final tabController = useTabController(initialLength: 3);
+
     return LayoutBuilder(
       builder: (context, constraints) {
         return Scaffold(
@@ -39,111 +42,51 @@ class CourtDetailsView extends StatelessWidget {
               Text(court.name.toUpperCase() + ((isAdmin) ? ' (ADMIN)' : '')),
               const SizedBox(height: 30),
               Expanded(
-                child: SfCalendar(
-                  view: CalendarView.schedule,
-                  dataSource: _getCalendarDataSource(),
-                  scheduleViewSettings: ScheduleViewSettings(
-                    appointmentItemHeight: constraints.maxHeight * 0.1,
-                  ),
-                  appointmentBuilder: (context, details) {
-                    return Column(
-                      children: details.appointments.map((a) {
-                        a as Appointment;
-                        final isDone = a.startTime.isBefore(DateTime.now());
-                        return Container(
-                          decoration: BoxDecoration(
-                            color:
-                                (isDone) ? Colors.grey.shade400 : Colors.green,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Material(
-                            color: Colors.transparent,
-                            child: InkWell(
-                              onTap: () {
-                                showDialog(
-                                  context: context,
-                                  builder: (context) {
-                                    return CourtSlotDetailsDialog(
-                                      constraints: constraints,
-                                      isAdmin: isAdmin,
-                                    );
-                                  },
-                                );
-                              },
-                              child: ListTile(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                textColor: Colors.white,
-                                title: const Text('Available'),
-                                subtitle: Text(
-                                    "${DateFormat('h:mm').format(a.startTime)} - ${DateFormat('h:mm a').format(a.endTime)}"),
-                                trailing: Column(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: const [
-                                    Text('Players'),
-                                    Text(
-                                      '0 / 15',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 25,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        );
-                      }).toList(),
-                    );
-                  },
+                child: TabBarView(
+                  physics: const NeverScrollableScrollPhysics(),
+                  controller: tabController,
+                  children: [
+                    CourtSchedulePanel(
+                      constraints: constraints,
+                      isAdmin: isAdmin,
+                    ),
+                    Container(),
+                  ],
                 ),
               ),
             ],
           ),
+          floatingActionButton: Visibility(
+            visible: isAdmin,
+            child: FloatingActionButton.extended(
+              onPressed: () {},
+              label: const Text('Add Admin'),
+              icon: const Icon(Icons.person),
+            ),
+          ),
+          bottomNavigationBar: Visibility(
+            visible: isAdmin,
+            child: BottomNavigationBar(
+              onTap: (index) {
+                tabIndex.value = index;
+                tabController.animateTo(index);
+              },
+              selectedItemColor: Colors.black,
+              currentIndex: tabController.index,
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.schedule),
+                  label: 'Schedule',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.people),
+                  label: 'Admins',
+                ),
+              ],
+            ),
+          ),
         );
       },
     );
-  }
-}
-
-_AppointmentDataSource _getCalendarDataSource() {
-  List<Appointment> appointments = <Appointment>[];
-  appointments.addAll([
-    Appointment(
-      startTime: DateTime(2015, 1, 1, 7, 0),
-      endTime: DateTime(2015, 1, 1, 8, 0),
-      recurrenceRule: SfCalendar.generateRRule(
-        RecurrenceProperties(
-          recurrenceType: RecurrenceType.daily,
-          startDate: DateTime.now(),
-        ),
-        DateTime(2015, 1, 1, 7, 0),
-        DateTime(2015, 1, 1, 8, 0),
-      ),
-    ),
-    Appointment(
-      startTime: DateTime(2015, 1, 1, 14, 0),
-      endTime: DateTime(2015, 1, 1, 15, 0),
-      subject: 'Players: 0 / 15',
-      recurrenceRule: SfCalendar.generateRRule(
-        RecurrenceProperties(
-          recurrenceType: RecurrenceType.daily,
-          startDate: DateTime.now(),
-        ),
-        DateTime(2015, 1, 1, 14, 0),
-        DateTime(2015, 1, 1, 15, 0),
-      ),
-    ),
-  ]);
-
-  return _AppointmentDataSource(appointments);
-}
-
-class _AppointmentDataSource extends CalendarDataSource {
-  _AppointmentDataSource(List<Appointment> source) {
-    appointments = source;
   }
 }
