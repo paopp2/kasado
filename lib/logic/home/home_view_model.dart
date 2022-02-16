8e77f8a35f199859ed0a9c6352974ea4ba6ea4c0
Paolo Pepito <npaolopepito@gmail.com>
2022-02-17 03:45:51 +0800

    Remove dependency to CourtRepo at HomeViewModel

--- lib/logic/home/home_view_model.dart
+++ lib/logic/home/home_view_model.dart
@@ -1,14 +1,11 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:kasado/data/repositories/court_repository.dart';
 import 'package:kasado/data/services/auth_service.dart';
 import 'package:kasado/logic/shared/view_model.dart';
-import 'package:kasado/model/court/court.dart';
 
 final homeViewModel = Provider.autoDispose(
   (ref) => HomeViewModel(
     read: ref.read,
     authService: ref.watch(authServiceProvider),
-    courtRepo: ref.watch(courtRepositoryProvider),
   ),
 );
 
@@ -16,13 +13,9 @@ class HomeViewModel extends ViewModel {
   HomeViewModel({
     required Reader read,
     required this.authService,
-    required this.courtRepo,
   }) : super(read);
 
   final AuthService authService;
-  final CourtRepository courtRepo;
 
   Future<void> signOut() async => await authService.signOutGoogle();
-
-  Stream<List<Court>> getCourtsStream() => courtRepo.getCourtsStream();
 }
--- lib/logic/home/states/home_tab_state.dart
+++ lib/logic/home/states/home_tab_state.dart
@@ -1,6 +1,12 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:kasado/logic/home/home_view_model.dart';
+import 'package:kasado/data/repositories/court_repository.dart';
+import 'package:kasado/model/court_slot/court_slot.dart';
 
 final courtsStreamProvider = StreamProvider.autoDispose(
-  (ref) => ref.watch(homeViewModel).getCourtsStream(),
+  (ref) => ref.watch(courtRepositoryProvider).getCourtsStream(),
+);
+
+final courtSlotsStreamProvider =
+    StreamProvider.autoDispose.family<List<CourtSlot>, String>(
+  (ref, courtId) => ref.watch(courtRepositoryProvider).getCourtSlots(courtId),
 );
