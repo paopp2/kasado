a6c55980ef6ec171e8f515e2a574e9a196148f95
Paolo Pepito <npaolopepito@gmail.com>
2022-02-16 14:15:22 +0800

    Refactor out UserInfoDrawer from HomeView

---
+++ lib/ui/home/components/user_info_drawer.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kasado/data/core/core_providers.dart';
+import 'package:kasado/logic/home/home_view_model.dart';
+
+class UserInfoDrawer extends HookConsumerWidget {
+  const UserInfoDrawer({
+    Key? key,
+    required this.constraints,
+    required this.model,
+  }) : super(key: key);
+
+  final BoxConstraints constraints;
+  final HomeViewModel model;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentUser = ref.watch(currentUserProvider);
+    return Drawer(
+      child: (currentUser == null)
+          ? const Center(child: Text("No user"))
+          : Column(
+              children: [
+                Container(
+                  height: constraints.maxHeight * 0.3,
+                  width: constraints.maxWidth * 0.85,
+                  color: const Color(0xfff5f5f5),
+                  padding: EdgeInsets.only(
+                    left: constraints.maxWidth * 0.05,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: constraints.maxHeight * 0.03,
+                        ),
+                        child: CircleAvatar(
+                          backgroundColor: const Color(0xFF5F45A4),
+                          radius: 48,
+                          child: CircleAvatar(
+                            radius: 45,
+                            backgroundImage:
+                                NetworkImage(currentUser.photoUrl!),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        currentUser.displayName!,
+                        overflow: TextOverflow.fade,
+                        softWrap: false,
+                      ),
+                      const Text("Forward | #14"),
+                    ],
+                  ),
+                ),
+                ListTile(
+                  onTap: model.signOut,
+                  title: const Text('Logout'),
+                  leading: const Icon(Icons.logout),
+                ),
+              ],
+            ),
+    );
+  }
+}
--- lib/ui/home/home_view.dart
+++ lib/ui/home/home_view.dart
@@ -4,6 +4,7 @@ import 'package:kasado/logic/home/home_view_model.dart';
 import 'package:kasado/model/court/court.dart';
 import 'package:kasado/model/court_slot/court_slot.dart';
 import 'package:kasado/ui/home/components/court_slot_card.dart';
+import 'package:kasado/ui/home/components/user_info_drawer.dart';
 
 class HomeView extends HookConsumerWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -12,62 +13,59 @@ class HomeView extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final model = ref.watch(homeViewModel);
 
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Scaffold(
-          appBar: AppBar(
-            leading: IconButton(
-              icon: const Icon(Icons.menu),
-              onPressed: () {},
+    return SafeArea(
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return Scaffold(
+            drawer: UserInfoDrawer(
+              constraints: constraints,
+              model: model,
             ),
-            actions: [
-              IconButton(
-                icon: const Icon(Icons.logout),
-                onPressed: model.signOut,
-              )
-            ],
-          ),
-          body: Center(
-            child: ListView(
-              children: [
-                const SizedBox(height: 50),
-                CourtSlotCard(
-                  model: model,
-                  constraints: constraints,
-                  courtSlot: CourtSlot(
-                    court: const Court(
-                      id: 'court1',
-                      name: 'Skina Japan Basketball Court',
-                      photoUrl:
-                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHonQRDPCLA0zXvTzoThs-Q2hQ9s5lg3RBmA&usqp=CAU",
-                      ticketPrice: 50,
-                    ),
-                    players: [],
-                    startsAt: DateTime(2022, 2, 16, 9, 0),
-                    endsAt: DateTime(2022, 2, 16, 10, 0),
-                  ),
-                ),
-                CourtSlotCard(
-                  model: model,
-                  constraints: constraints,
-                  courtSlot: CourtSlot(
-                    court: const Court(
-                      id: 'court2',
-                      name: 'Skina China Basketball Court',
-                      photoUrl:
-                          "https://i.pinimg.com/originals/80/9f/af/809faf105f55a2830918b9d859bd3958.jpg",
-                      ticketPrice: 15,
-                    ),
-                    players: [],
-                    startsAt: DateTime(2022, 2, 16, 8, 0),
-                    endsAt: DateTime(2022, 2, 16, 9, 0),
-                  ),
-                ),
-              ],
+            appBar: AppBar(
+              elevation: 0,
+              backgroundColor: Colors.transparent,
             ),
-          ),
-        );
-      },
+            body: Center(
+              child: ListView(
+                children: [
+                  CourtSlotCard(
+                    model: model,
+                    constraints: constraints,
+                    courtSlot: CourtSlot(
+                      court: const Court(
+                        id: 'court1',
+                        name: 'Skina Japan Basketball Court',
+                        photoUrl:
+                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHonQRDPCLA0zXvTzoThs-Q2hQ9s5lg3RBmA&usqp=CAU",
+                        ticketPrice: 50,
+                      ),
+                      players: [],
+                      startsAt: DateTime(2022, 2, 16, 9, 0),
+                      endsAt: DateTime(2022, 2, 16, 10, 0),
+                    ),
+                  ),
+                  CourtSlotCard(
+                    model: model,
+                    constraints: constraints,
+                    courtSlot: CourtSlot(
+                      court: const Court(
+                        id: 'court2',
+                        name: 'Skina China Basketball Court',
+                        photoUrl:
+                            "https://i.pinimg.com/originals/80/9f/af/809faf105f55a2830918b9d859bd3958.jpg",
+                        ticketPrice: 15,
+                      ),
+                      players: [],
+                      startsAt: DateTime(2022, 2, 16, 8, 0),
+                      endsAt: DateTime(2022, 2, 16, 9, 0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
