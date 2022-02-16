ce3106af79e08789e7bec19b55f3e0f655b2e610
Paolo Pepito <npaolopepito@gmail.com>
2022-02-17 01:40:59 +0800

    Add configurations for enabling Firestore emulator

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kasado/app_router.dart';
 import 'package:kasado/firebase_options.dart';
 
+// TODO: Set to false in production
+const bool useFirebaseEmulator = true;
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  if (useFirebaseEmulator) {
+    final localHostString = Platform.isAndroid ? '10.0.2.2' : 'localhost';
+    FirebaseFirestore.instance.settings = Settings(
+      host: '$localHostString:8080',
+      sslEnabled: false,
+      persistenceEnabled: false,
+    );
+  }
 
   runApp(const ProviderScope(child: MyApp()));
 }
