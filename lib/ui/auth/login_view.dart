b3f6afb91a3004794d8995bfdafb96ccfc0c88b7
Paolo Pepito <npaolopepito@gmail.com>
2022-02-17 21:24:18 +0800

    Add some more temp design for LoginView

--- lib/ui/auth/login_view.dart
+++ lib/ui/auth/login_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kasado/logic/auth/login_view_model.dart';
 
@@ -14,12 +15,23 @@ class LoginView extends HookConsumerWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            const Text(
+              'KASADO',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(width: 200, child: Divider()),
             TextButton(
               onPressed: model.signInWithGoogle,
               child: const Text('Sign in with Google'),
             ),
             TextButton(
-              onPressed: () {},
+              onPressed: () => Fluttertoast.showToast(
+                msg:
+                    'Google lng say gamita pre, butngan ra nya nako nig para FB pramis',
+              ),
               child: const Text('Sign in with Facebook'),
             ),
           ],
