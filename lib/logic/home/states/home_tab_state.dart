00efd80a65822e97f1dc9360da6844535caf941c
Paolo Pepito <npaolopepito@gmail.com>
2022-02-17 11:20:58 +0800

    Remove unused import at home_tab_state

--- lib/logic/home/states/home_tab_state.dart
+++ lib/logic/home/states/home_tab_state.dart
@@ -1,6 +1,5 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kasado/data/repositories/court_repository.dart';
-import 'package:kasado/model/court_slot/court_slot.dart';
 
 final courtsStreamProvider = StreamProvider.autoDispose(
   (ref) => ref.watch(courtRepositoryProvider).getCourtsStream(),
