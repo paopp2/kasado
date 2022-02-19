0682c3ca144ac5a166af8ebf9da758a0df41defa
Paolo Pepito <npaolopepito@gmail.com>
2022-02-19 08:16:51 +0800

    Add paths for FirestorePath

--- lib/data/helpers/firestore_path.dart
+++ lib/data/helpers/firestore_path.dart
@@ -4,9 +4,11 @@ class FirestorePath {
   // COLLECTION paths
   static String colCourts() => 'courts';
   static String colCourtSlots(String courtId) => 'courts/$courtId/court_slots';
+  static String colUserInfos() => 'user_info';
 
   // DOCUMENT paths
   static String docCourt(String courtId) => 'courts/$courtId';
   static String docCourtSlot(String courtId, String slotId) =>
       'courts/$courtId/court_slots/$slotId';
+  static String docUserInfo(String userId) => 'user_info/$userId';
 }
