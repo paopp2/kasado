6cc2bb389afb5667587943477ba3ae8723d5b7fa
Paolo Pepito <npaolopepito@gmail.com>
2022-02-23 23:22:57 +0800

    Set urlStrategy for app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kasado/app_router.dart';
 import 'package:kasado/firebase_options.dart';
@@ -26,6 +27,8 @@ Future<void> main() async {
       persistenceEnabled: false,
     );
   }
+
+  GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
 
   runApp(const ProviderScope(child: MyApp()));
 }
