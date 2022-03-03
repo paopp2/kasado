55a6be463cb51b2718730cdd7ab963c4de64ba04
Paolo Pepito <npaolopepito@gmail.com>
2022-03-03 12:48:54 +0800

    Add design skeleton for NotifsView

--- lib/ui/notifs/notifs_view.dart
+++ lib/ui/notifs/notifs_view.dart
@@ -17,7 +17,65 @@ class NotifsView extends StatelessWidget {
             backgroundColor: Colors.transparent,
             elevation: 0,
           ),
-          body: Container(color: (isAdmin) ? Colors.red : Colors.blueGrey),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView.builder(
+              itemBuilder: (context, i) {
+                final isEven = i % 2 == 0;
+                return ListTile(
+                  contentPadding: const EdgeInsets.all(8),
+                  tileColor:
+                      (isEven) ? Colors.blue.shade100.withAlpha(50) : null,
+                  leading: const CircleAvatar(
+                    backgroundColor: Colors.amber,
+                    radius: 25,
+                  ),
+                  title: const Text(
+                    'This is the title',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text(
+                        'The quic brown fox umpso ver the lazy doThe quic brown fox umpso ver the lazy doThe quic brown fox umpso ver the lazy doThe quic brown fox umpso ver the lazy doThe quic brown fox umpso ver the lazy doggggg The quic brown fox umpso ver the lazy doThe quic brown fox umpso ver the lazy doThe quic brown fox umpso ver the lazy doThe quic brown fox umpso ver the lazy doThe quic brown fox umpso ver the lazy doggggg',
+                      ),
+                      Visibility(
+                        visible: true,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            IconButton(
+                              icon: Icon(
+                                Icons.thumb_down,
+                                color: Colors.red.shade300,
+                              ),
+                              onPressed: () {},
+                            ),
+                            IconButton(
+                              icon: Icon(
+                                Icons.thumb_up,
+                                color: Colors.green.shade300,
+                              ),
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          floatingActionButton: Visibility(
+            visible: isAdmin,
+            child: FloatingActionButton.extended(
+              label: const Text('New notif'),
+              icon: const Icon(Icons.add),
+              onPressed: () {},
+            ),
+          ),
         );
       },
     );
