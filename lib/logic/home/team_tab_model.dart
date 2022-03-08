6e5e96ca116b30159177ce8c0ee0656be7184231
Paolo Pepito <npaolopepito@gmail.com>
2022-03-08 21:18:52 +0800

    Extend ViewModel for TeamTabModel

--- lib/logic/home/team_tab_model.dart
+++ lib/logic/home/team_tab_model.dart
@@ -1,17 +1,17 @@
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kasado/logic/home/states/team_tab_state.dart';
+import 'package:kasado/logic/shared/view_model.dart';
 import 'package:kasado/model/kasado_user_info/kasado_user_info.dart';
 
 final teamTabModel = Provider.autoDispose(
   (ref) => TeamTabModel(read: ref.read),
 );
 
-class TeamTabModel {
-  const TeamTabModel({
-    required this.read,
-  });
-  final Reader read;
+class TeamTabModel extends ViewModel {
+  TeamTabModel({
+    required Reader read,
+  }) : super(read);
 
   void addUserInfoToTeam(KasadoUserInfo userInfo) {
     read(teamUserInfoListProvider.notifier).update(
