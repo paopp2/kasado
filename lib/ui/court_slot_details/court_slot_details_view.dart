47a49597e34bdd936f3e470c54b3948450286bc2
Paolo Pepito <npaolopepito@gmail.com>
2022-03-19 23:37:52 +0800

    WIP: Placeholder UI for StatsController

--- lib/ui/court_slot_details/court_slot_details_view.dart
+++ lib/ui/court_slot_details/court_slot_details_view.dart
@@ -348,7 +348,114 @@ class CourtSlotDetailsView extends HookConsumerWidget {
                               ),
                             ],
                           ),
-                          if (isAdmin) ...[Container()],
+                          if (isAdmin) ...[
+                            Column(
+                              children: [
+                                const Expanded(
+                                  child: Center(child: Text('Hi po')),
+                                ),
+                                Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        ElevatedButton(
+                                          child: const Text('3'),
+                                          onPressed: () {},
+                                          style: ElevatedButton.styleFrom(
+                                            primary: Colors.green,
+                                            fixedSize: const Size(75, 75),
+                                            shape: const CircleBorder(),
+                                          ),
+                                        ),
+                                        ElevatedButton(
+                                          child: const Text('3'),
+                                          onPressed: () {},
+                                          style: ElevatedButton.styleFrom(
+                                            primary: Colors.red,
+                                            fixedSize: const Size(75, 75),
+                                            shape: const CircleBorder(),
+                                          ),
+                                        ),
+                                        const Spacer(),
+                                        ElevatedButton(
+                                          child: const Text('OREB'),
+                                          onPressed: () {},
+                                          style: ElevatedButton.styleFrom(
+                                            primary: Colors.red,
+                                            fixedSize: const Size(75, 75),
+                                            shape: const CircleBorder(),
+                                          ),
+                                        ),
+                                        ElevatedButton(
+                                          child: const Text('DREB'),
+                                          onPressed: () {},
+                                          style: ElevatedButton.styleFrom(
+                                            primary: Colors.green,
+                                            fixedSize: const Size(75, 75),
+                                            shape: const CircleBorder(),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        ElevatedButton(
+                                          child: const Text('2'),
+                                          onPressed: () {},
+                                          style: ElevatedButton.styleFrom(
+                                            primary: Colors.green,
+                                            fixedSize: const Size(75, 75),
+                                            shape: const CircleBorder(),
+                                          ),
+                                        ),
+                                        ElevatedButton(
+                                          child: const Text('2'),
+                                          onPressed: () {},
+                                          style: ElevatedButton.styleFrom(
+                                            primary: Colors.red,
+                                            fixedSize: const Size(75, 75),
+                                            shape: const CircleBorder(),
+                                          ),
+                                        ),
+                                        const Spacer(),
+                                        ElevatedButton(
+                                          child: const Text('STL'),
+                                          onPressed: () {},
+                                          style: ElevatedButton.styleFrom(
+                                            primary: Colors.red,
+                                            fixedSize: const Size(75, 75),
+                                            shape: const CircleBorder(),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        ElevatedButton(
+                                          child: const Text('1'),
+                                          onPressed: () {},
+                                          style: ElevatedButton.styleFrom(
+                                            primary: Colors.green,
+                                            fixedSize: const Size(75, 75),
+                                            shape: const CircleBorder(),
+                                          ),
+                                        ),
+                                        ElevatedButton(
+                                          child: const Text('1'),
+                                          onPressed: () {},
+                                          style: ElevatedButton.styleFrom(
+                                            primary: Colors.red,
+                                            fixedSize: const Size(75, 75),
+                                            shape: const CircleBorder(),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
                         ],
                       ),
                     ),
