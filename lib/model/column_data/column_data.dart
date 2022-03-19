4f16e86deaf390215a43321513f70dadfc303088
Paolo Pepito <npaolopepito@gmail.com>
2022-03-19 20:33:00 +0800

    Add KasadoTable and ColumnData to use for showing stats

---
+++ lib/model/column_data/column_data.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+
+class ColumnData<T extends Object?> {
+  ColumnData({
+    required this.columnName,
+    required this.dataValueCallback,
+    required this.valueToStringCallback,
+    this.contentAlignment = Alignment.center,
+    this.columnWidthMode = ColumnWidthMode.none,
+    this.isVisible = true,
+    this.headerTooltip = '',
+    this.contentTooltip,
+  });
+
+  final String columnName;
+  final String headerTooltip;
+  final String Function(Object?, T)? contentTooltip;
+  final Alignment contentAlignment;
+  final Object? Function(T) dataValueCallback;
+  final String Function(Object?, T) valueToStringCallback;
+  final ColumnWidthMode columnWidthMode;
+  final bool isVisible;
+}
