7ef8bb37d7bc041be1e98c194b24134b7216b6d1
Paolo Pepito <npaolopepito@gmail.com>
2022-03-19 12:24:44 +0800

    Add some cloud functions and test data for debugging

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,26 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const user_info_data = require("./test_data/user_info.json");
+const courts_data = require("./test_data/courts.json");
+const test_data = Object.assign({},courts_data, user_info_data);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const db = admin.firestore();
+const userInfoRef = db.collection('user_info');
+const courtsRef = db.collection('courts');
+
+exports.populateUserInfo = functions.https.onRequest(async (req, res) =>  {
+    test_data.userInfoList.forEach(addUserInfo);
+    async function addUserInfo(userInfo) {
+        await userInfoRef.doc(userInfo.id).set(userInfo);
+    }
+    res.json({result: "Populated 'user_info'"});
+});
+
+exports.populateCourts = functions.https.onRequest(async (req, res) =>  {
+    test_data.courtsList.forEach(addCourt);
+    async function addCourt(court) {
+        await courtsRef.doc(court.id).set(court);
+    }
+    res.json({result: "Populated 'courts'"});
+});
