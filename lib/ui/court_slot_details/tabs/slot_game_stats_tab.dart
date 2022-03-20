0b2f0fbcbf61ea0eafa399f1e30aa68acbc894f1
Paolo Pepito <npaolopepito@gmail.com>
2022-03-20 16:54:25 +0800

    WIP: Implement selecting of game stats to listen to at SlotGameStatsTab

--- lib/ui/court_slot_details/tabs/slot_game_stats_tab.dart
+++ lib/ui/court_slot_details/tabs/slot_game_stats_tab.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kasado/logic/admin/stat_manager/game_stat_state.dart';
 import 'package:kasado/model/column_data/column_data.dart';
 import 'package:kasado/model/court_slot/court_slot.dart';
+import 'package:kasado/model/game_stats/game_stats.dart';
 import 'package:kasado/ui/shared/kasado_table.dart';
 import 'package:kasado/ui/shared/loading_widget.dart';
 
@@ -20,105 +22,108 @@ class SlotGameStatsTab extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final allGameStatsStream = ref.watch(allSlotGamesStatsStreamProvider(
         '${courtSlot.courtId}|${courtSlot.slotId}'));
+    final selectedGameStatsState = useState<GameStats?>(null);
 
     return Column(
       children: [
         Expanded(
-          child: ListView(
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  "HOME",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
+          child: (selectedGameStatsState.value == null)
+              ? const Text('Hello po')
+              : ListView(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        selectedGameStatsState.value!.id,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    KasadoTable<String>(
+                      height: constraints.maxHeight * 0.5,
+                      width: constraints.maxWidth,
+                      frozenColumnCount: 1,
+                      dataAsList: const [
+                        "Hello",
+                        "Po",
+                        "Hello",
+                        "Po",
+                        "Hello",
+                      ],
+                      columnDataList: [
+                        ColumnData(
+                          columnName: "Name",
+                          dataValueCallback: (val) => val,
+                          valueToStringCallback: (val, valu) => valu,
+                        ),
+                        ColumnData(
+                          columnName: "Name 2",
+                          dataValueCallback: (val) => val,
+                          valueToStringCallback: (val, valu) => valu,
+                        ),
+                        ColumnData(
+                          columnName: "Name 3",
+                          dataValueCallback: (val) => val,
+                          valueToStringCallback: (val, valu) => valu,
+                        ),
+                        ColumnData(
+                          columnName: "Name 4",
+                          dataValueCallback: (val) => val,
+                          valueToStringCallback: (val, valu) => valu,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 30),
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        "AWAY",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    KasadoTable<String>(
+                      height: constraints.maxHeight,
+                      width: constraints.maxWidth,
+                      frozenColumnCount: 1,
+                      dataAsList: const [
+                        "Hello",
+                        "Po",
+                        "Hello",
+                        "Po",
+                        "Hello",
+                      ],
+                      columnDataList: [
+                        ColumnData(
+                          columnName: "Name",
+                          dataValueCallback: (val) => val,
+                          valueToStringCallback: (val, valu) => valu,
+                        ),
+                        ColumnData(
+                          columnName: "Name 2",
+                          dataValueCallback: (val) => val,
+                          valueToStringCallback: (val, valu) => valu,
+                        ),
+                        ColumnData(
+                          columnName: "Name 3",
+                          dataValueCallback: (val) => val,
+                          valueToStringCallback: (val, valu) => valu,
+                        ),
+                        ColumnData(
+                          columnName: "Name 4",
+                          dataValueCallback: (val) => val,
+                          valueToStringCallback: (val, valu) => valu,
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ),
-              KasadoTable<String>(
-                height: constraints.maxHeight * 0.5,
-                width: constraints.maxWidth,
-                frozenColumnCount: 1,
-                dataAsList: const [
-                  "Hello",
-                  "Po",
-                  "Hello",
-                  "Po",
-                  "Hello",
-                ],
-                columnDataList: [
-                  ColumnData(
-                    columnName: "Name",
-                    dataValueCallback: (val) => val,
-                    valueToStringCallback: (val, valu) => valu,
-                  ),
-                  ColumnData(
-                    columnName: "Name 2",
-                    dataValueCallback: (val) => val,
-                    valueToStringCallback: (val, valu) => valu,
-                  ),
-                  ColumnData(
-                    columnName: "Name 3",
-                    dataValueCallback: (val) => val,
-                    valueToStringCallback: (val, valu) => valu,
-                  ),
-                  ColumnData(
-                    columnName: "Name 4",
-                    dataValueCallback: (val) => val,
-                    valueToStringCallback: (val, valu) => valu,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 30),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  "AWAY",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              KasadoTable<String>(
-                height: constraints.maxHeight,
-                width: constraints.maxWidth,
-                frozenColumnCount: 1,
-                dataAsList: const [
-                  "Hello",
-                  "Po",
-                  "Hello",
-                  "Po",
-                  "Hello",
-                ],
-                columnDataList: [
-                  ColumnData(
-                    columnName: "Name",
-                    dataValueCallback: (val) => val,
-                    valueToStringCallback: (val, valu) => valu,
-                  ),
-                  ColumnData(
-                    columnName: "Name 2",
-                    dataValueCallback: (val) => val,
-                    valueToStringCallback: (val, valu) => valu,
-                  ),
-                  ColumnData(
-                    columnName: "Name 3",
-                    dataValueCallback: (val) => val,
-                    valueToStringCallback: (val, valu) => valu,
-                  ),
-                  ColumnData(
-                    columnName: "Name 4",
-                    dataValueCallback: (val) => val,
-                    valueToStringCallback: (val, valu) => valu,
-                  ),
-                ],
-              ),
-            ],
-          ),
         ),
         const Divider(),
         SingleChildScrollView(
@@ -127,6 +132,15 @@ class SlotGameStatsTab extends HookConsumerWidget {
               error: (e, _) => Text(e.toString()),
               loading: () => const LoadingWidget(),
               data: (allGameStats) {
+                if (allGameStats.isNotEmpty &&
+                    (selectedGameStatsState.value == null)) {
+                  // If gameStats is not empty and no game stats had been
+                  // selected yet, show the stats of the first game by default.
+                  // Update asynchronously to avoid UI rebuild errors or "clashes"
+                  Future.delayed(Duration.zero, () {
+                    selectedGameStatsState.value = allGameStats.first;
+                  });
+                }
                 return Row(
                   children: allGameStats
                       .asMap()
@@ -135,12 +149,16 @@ class SlotGameStatsTab extends HookConsumerWidget {
                         (gameStatEntry) => TextButton(
                           child: Text(
                             'G${gameStatEntry.key + 1}',
-                            style: const TextStyle(
-                              color: Colors.green,
+                            style: TextStyle(
+                              color: (selectedGameStatsState.value ==
+                                      gameStatEntry.value)
+                                  ? Colors.green
+                                  : null,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
-                          onPressed: () {},
+                          onPressed: () => (selectedGameStatsState.value =
+                              gameStatEntry.value),
                         ),
                       )
                       .toList(),
