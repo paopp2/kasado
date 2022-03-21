b75810e64a9ad31453e49281a4f6e820bfa693a9
Paolo Pepito <npaolopepito@gmail.com>
2022-03-21 13:16:19 +0800

    Show user's overview stats at UserProfilePane tab

--- lib/ui/shared/profile_widgets/user_profile_pane.dart
+++ lib/ui/shared/profile_widgets/user_profile_pane.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kasado/data/core/core_providers.dart';
+import 'package:kasado/logic/shared/kasado_utils.dart';
 import 'package:kasado/model/kasado_user_info/kasado_user_info.dart';
 import 'package:kasado/ui/home/components/pondo_info_dialog.dart';
-import 'package:kasado/ui/shared/profile_widgets/player_stat_block.dart';
+import 'package:kasado/ui/shared/profile_widgets/player_stat_tile.dart';
 
 class UserProfilePane extends HookConsumerWidget {
   const UserProfilePane({
@@ -18,10 +19,12 @@ class UserProfilePane extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final currentUser = ref.watch(currentUserProvider)!;
+    final utils = ref.watch(kasadoUtilsProvider);
     final user = userInfo?.user;
     final isCurrentUser = currentUser.id == user?.id;
     final isSuperAdmin =
         ref.watch(currentUserInfoProvider).value?.isSuperAdmin ?? false;
+    final userStats = userInfo?.overviewStats;
 
     return (user == null)
         ? const Text('No user info')
@@ -68,12 +71,90 @@ class UserProfilePane extends HookConsumerWidget {
                   ],
                 ),
               ),
-              PlayerStatBlock(
-                constraints: constraints,
-                statValue:
-                    userInfo?.overviewStats.mvpVoteCount.toString() ?? '0',
-                statDescription: 'MVP Votes',
-              )
+              Expanded(
+                child: (userStats == null)
+                    ? const Center(child: Text('No stats available'))
+                    : Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 40,
+                          vertical: 8,
+                        ),
+                        child: ListView(
+                          children: [
+                            PlayerStatTile(
+                              statDescription: "GAMES PLAYED",
+                              statValue: userStats.gamesPlayed.toString(),
+                            ),
+                            PlayerStatTile(
+                              statDescription: "STANDING",
+                              statValue:
+                                  "${userStats.totalWins} - ${userStats.totalLosses}",
+                            ),
+                            PlayerStatTile(
+                              statDescription: "WIN RATE",
+                              statValue: utils
+                                  .getPercentageFormat(userStats.winPercent),
+                            ),
+                            PlayerStatTile(
+                              statDescription: "POINTS PER GAME",
+                              statValue:
+                                  userStats.avePointsPerGame.toStringAsFixed(1),
+                            ),
+                            PlayerStatTile(
+                              statDescription: "ASSISTS PER GAME",
+                              statValue: userStats.aveAssistsPerGame
+                                  .toStringAsFixed(1),
+                            ),
+                            PlayerStatTile(
+                              statDescription: "REBOUNDS PER GAME",
+                              statValue: userStats.aveReboundsPerGame
+                                  .toStringAsFixed(1),
+                            ),
+                            PlayerStatTile(
+                              statDescription: "BLOCKS PER GAME",
+                              statValue:
+                                  userStats.aveBlocksPerGame.toStringAsFixed(1),
+                            ),
+                            PlayerStatTile(
+                              statDescription: "STEALS PER GAME",
+                              statValue:
+                                  userStats.aveStlPerGame.toStringAsFixed(1),
+                            ),
+                            PlayerStatTile(
+                              statDescription: "TOTAL POINTS",
+                              statValue: userStats.totalPoints.toString(),
+                            ),
+                            PlayerStatTile(
+                              statDescription: "TOTAL ASSISTS",
+                              statValue: userStats.totalAst.toString(),
+                            ),
+                            PlayerStatTile(
+                              statDescription: "TOTAL REBOUNDS",
+                              statValue: userStats.totalRebounds.toString(),
+                            ),
+                            PlayerStatTile(
+                              statDescription: "TOTAL BLOCKS",
+                              statValue: userStats.totalBlk.toString(),
+                            ),
+                            PlayerStatTile(
+                              statDescription: "TOTAL STEALS",
+                              statValue: userStats.totalStl.toString(),
+                            ),
+                            PlayerStatTile(
+                              statDescription: "FG %",
+                              statValue: utils
+                                  .getPercentageFormat(userStats.aveFgPercent),
+                            ),
+                            PlayerStatTile(
+                              statDescription: "3PT %",
+                              statValue: utils.getPercentageFormat(
+                                userStats.aveThreePtPercent,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+              ),
             ],
           );
   }
