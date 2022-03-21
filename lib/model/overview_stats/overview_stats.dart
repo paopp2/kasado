7e5e8921b0777c1dc64d7a8eea28e167733890a0
Paolo Pepito <npaolopepito@gmail.com>
2022-03-21 08:21:14 +0800

    Add new lifetime stats

--- lib/model/overview_stats/overview_stats.dart
+++ lib/model/overview_stats/overview_stats.dart
@@ -9,7 +9,28 @@ class OverviewStats with _$OverviewStats {
   const OverviewStats._();
   const factory OverviewStats({
     @Default(0) int mvpVoteCount,
+    @Default(0) int totalThreePA,
+    @Default(0) int totalThreePM,
+    @Default(0) int totalTwoPA,
+    @Default(0) int totalTwoPM,
+    @Default(0) int totalFta,
+    @Default(0) int totalFtm,
+    @Default(0) int totalOReb,
+    @Default(0) int totalDReb,
+    @Default(0) int totalAst,
+    @Default(0) int totalStl,
+    @Default(0) int totalBlk,
+    @Default(0) int gamesPlayed,
   }) = _OverviewStats;
+
+  int get totalPoints => (totalThreePM * 3) + (totalTwoPM * 2) + totalFtm;
+  int get totalAttempts => totalThreePA + totalTwoPA;
+  int get totalMade => totalThreePM + totalTwoPM;
+  int get totalRebounds => totalOReb + totalDReb;
+  double get aveFgPercent => (totalMade / totalAttempts) * 100;
+  double get aveThreePtPercent => (totalThreePM / totalThreePA) * 100;
+  double get aveFtPercent => (totalFtm / totalFta) * 100;
+  double get avePointsPerGame => totalPoints / gamesPlayed;
 
   factory OverviewStats.fromJson(Map<String, dynamic> json) =>
       _$OverviewStatsFromJson(json);
