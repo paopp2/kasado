84488d07d2fcfa3f43491b26974274ea69914f1f
Paolo Pepito <npaolopepito@gmail.com>
2022-04-06 06:55:09 +0800

    Update KasadoUserInfo model to use 'tickets' instead of 'reservedAt'

--- lib/model/kasado_user_info/kasado_user_info.dart
+++ lib/model/kasado_user_info/kasado_user_info.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:kasado/model/court_slot/court_slot.dart';
 import 'package:kasado/model/kasado_user/kasado_user.dart';
 import 'package:kasado/model/overview_stats/overview_stats.dart';
+import 'package:kasado/model/ticket/ticket.dart';
 
 part 'kasado_user_info.freezed.dart';
 part 'kasado_user_info.g.dart';
@@ -19,20 +20,34 @@ class KasadoUserInfo with _$KasadoUserInfo {
     @Default(false) bool isTeamCaptain,
     @Default(OverviewStats()) OverviewStats overviewStats,
     String? teamId,
-    CourtSlot? reservedAt,
+    @Default([]) List<Ticket> tickets,
   }) = _KasadoUserInfo;
 
   bool get hasReserved {
-    if (reservedAt == null) return false;
+    if (tickets.isEmpty) return false;
 
     final now = DateTime.now();
-    // 1 hour after a game slot starts, only then can a user join another slot.
-    // Hence, the user by that time has NOT reserved
-    final isReservedAtFuture = reservedAt?.timeRange.startsAt
-            .add(const Duration(hours: 1))
-            .isAfter(now) ??
-        false;
-    return isReservedAtFuture;
+    // Check if user has any pending tickets reserved for the future.
+    // The reason for adding 1hr is because a slot is considered to be done only
+    // after an hour from when it started. Might have to refactor in the future.
+    return tickets.any(
+      (ticket) => ticket.courtSlot.timeRange.startsAt
+          .add(const Duration(hours: 1))
+          .isAfter(now),
+    );
+  }
+
+  bool hasSchedConflict(CourtSlot courtSlot) {
+    return tickets.any(
+      (ticket) => ticket.courtSlot.timeRange.startsAt
+          .isAtSameMomentAs(courtSlot.timeRange.startsAt),
+    );
+  }
+
+  bool isReservedHere(CourtSlot courtSlot) {
+    return tickets.any((ticket) =>
+        (ticket.courtSlot.courtId == courtSlot.courtId) &&
+        (ticket.courtSlot.slotId == courtSlot.slotId));
   }
 
   bool get hasTeam => (teamId != null);
