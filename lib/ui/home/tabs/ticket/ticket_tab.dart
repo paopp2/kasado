de849016d2e95c3260c21dafbb916f7b7af8518c
Paolo Pepito <npaolopepito@gmail.com>
2022-04-06 09:09:15 +0800

    WIP: Show a list of all tickets at TicketTab

--- lib/ui/home/tabs/ticket/ticket_tab.dart
+++ lib/ui/home/tabs/ticket/ticket_tab.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kasado/data/core/core_providers.dart';
+import 'package:kasado/logic/shared/kasado_utils.dart';
 import 'package:kasado/ui/shared/loading_widget.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
@@ -14,64 +15,83 @@ class TicketTab extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final currentUser = ref.watch(currentUserProvider);
     final currentUserInfoStream = ref.watch(currentUserInfoProvider);
+    final utils = ref.watch(kasadoUtilsProvider);
 
     return currentUserInfoStream.when(
       error: (e, _) => Text(e.toString()),
       loading: () => const LoadingWidget(),
       data: (userInfo) {
+        final tickets = userInfo?.tickets ?? [];
         return Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30),
-            ),
-            elevation: 20,
-            child: !(userInfo?.hasReserved ?? false)
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text('NO TICKET'),
-                      Text('(Join a game to obtain a ticket)'),
-                    ],
-                  )
-                : Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'GAME TICKET',
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                        ),
+          child: tickets.isEmpty
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text('NO TICKETS'),
+                    Text('(Join games to obtain tickets)'),
+                  ],
+                )
+              : Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'YOUR TICKETS',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: QrImage(
-                          data:
-                              'Eacha prank! Bitaw dol mugana ra ni puhon trust me',
-                        ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: tickets.length,
+                        itemBuilder: (context, i) {
+                          final ticket = tickets[i];
+                          return Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: ListTile(
+                              tileColor: Colors.grey.shade100,
+                              leading: QrImage(
+                                data:
+                                    'Eacha prank! Bitaw dol mugana ra ni puhon trust me',
+                              ),
+                              title: Text(ticket.courtName),
+                              subtitle: Text(
+                                "${utils.getDateFormat(ticket.courtSlot.timeRange.startsAt)} / ${utils.getTimeRangeFormat(ticket.courtSlot.timeRange)}",
+                              ),
+                            ),
+                          );
+                        },
                       ),
-                      const Divider(),
-                      Text(
-                        currentUser!.displayName!.toUpperCase(),
-                        style: const TextStyle(fontSize: 10),
-                      ),
-                      const Text(
-                        // userInfo!.reservedAt!.courtId,
-                        'Placeholder',
-                        style: TextStyle(fontSize: 10),
-                      ),
-                      const Text(
-                        // userInfo.reservedAt!.slotId,
-                        'Placeholder',
-                        style: TextStyle(fontSize: 10),
-                      ),
-                    ],
-                  ),
-          ),
+                    ),
+                    // Padding(
+                    //   padding: const EdgeInsets.all(20.0),
+                    //   child: QrImage(
+                    //     data:
+                    //         'Eacha prank! Bitaw dol mugana ra ni puhon trust me',
+                    //   ),
+                    // ),
+                    // const Divider(),
+                    // Text(
+                    //   currentUser!.displayName!.toUpperCase(),
+                    //   style: const TextStyle(fontSize: 10),
+                    // ),
+                    // const Text(
+                    //   // userInfo!.reservedAt!.courtId,
+                    //   'Placeholder',
+                    //   style: TextStyle(fontSize: 10),
+                    // ),
+                    // const Text(
+                    //   // userInfo.reservedAt!.slotId,
+                    //   'Placeholder',
+                    //   style: TextStyle(fontSize: 10),
+                    // ),
+                  ],
+                ),
         );
       },
     );
