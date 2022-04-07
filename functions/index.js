b2b66514e70f5884d3a77fd71baae13a0a0c677f
Paolo Pepito <npaolopepito@gmail.com>
2022-04-07 12:22:19 +0800

    Implement cloud function for deriveable stats calculation on user stats update

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 const user_info_data = require("./test_data/user_info.json");
 const courts_data = require("./test_data/courts.json");
 const teams_data = require("./test_data/teams.json");
-const test_data = Object.assign({},teams_data, courts_data, user_info_data);
+const test_data = Object.assign({}, teams_data, courts_data, user_info_data);
 
 admin.initializeApp();
 const db = admin.firestore();
@@ -11,7 +11,7 @@ const userInfoRef = db.collection('user_info');
 const courtsRef = db.collection('courts');
 const teamsRef = db.collection('teams');
 
-exports.populateAll = functions.https.onRequest(async (req, res) =>  {
+exports.populateAll = functions.https.onRequest(async (req, res) => {
     test_data.userInfoList.forEach(addUserInfo);
     async function addUserInfo(userInfo) {
         await userInfoRef.doc(userInfo.id).set(userInfo);
@@ -24,29 +24,80 @@ exports.populateAll = functions.https.onRequest(async (req, res) =>  {
     async function addTeam(team) {
         await teamsRef.doc(team.id).set(team);
     }
-    res.json({result: "Populated ALL"});
+    res.json({ result: "Populated ALL" });
 });
 
-exports.populateUserInfo = functions.https.onRequest(async (req, res) =>  {
+exports.populateUserInfo = functions.https.onRequest(async (req, res) => {
     test_data.userInfoList.forEach(addUserInfo);
     async function addUserInfo(userInfo) {
         await userInfoRef.doc(userInfo.id).set(userInfo);
     }
-    res.json({result: "Populated 'user_info'"});
+    res.json({ result: "Populated 'user_info'" });
 });
 
-exports.populateCourts = functions.https.onRequest(async (req, res) =>  {
+exports.populateCourts = functions.https.onRequest(async (req, res) => {
     test_data.courtsList.forEach(addCourt);
     async function addCourt(court) {
         await courtsRef.doc(court.id).set(court);
     }
-    res.json({result: "Populated 'courts'"});
+    res.json({ result: "Populated 'courts'" });
 });
 
-exports.populateTeams = functions.https.onRequest(async (req, res) =>  {
+exports.populateTeams = functions.https.onRequest(async (req, res) => {
     test_data.teamsList.forEach(addTeam);
     async function addTeam(team) {
         await teamsRef.doc(court.id).set(team);
     }
-    res.json({result: "Populated 'teams'"});
+    res.json({ result: "Populated 'teams'" });
+});
+
+exports.calcDeriveableStats = functions.firestore.document('user_info/{userId}').onUpdate(async (change, context) => {
+    const preUserInfo = change.before.data();
+    const updatedUserInfo = change.after.data();
+    if (updatedUserInfo && updatedUserInfo.overviewStats && updatedUserInfo.overviewStats.hasOwnProperty('gamesPlayed')) {
+        let preGamesPlayed;
+        try {
+            preGamesPlayed = preUserInfo.overviewStats.gamesPlayed;
+        } catch {
+            preGamesPlayed = 0;
+        }
+        if (preGamesPlayed != updatedUserInfo.overviewStats.gamesPlayed) {
+
+            const stats = updatedUserInfo.overviewStats;
+            const totalPoints = (stats.totalThreePM * 3) + (stats.totalTwoPM * 2) + stats.totalFtm;
+            const totalAttempts = stats.totalThreePA + stats.totalTwoPA;
+            const totalMade = stats.totalThreePM + stats.totalTwoPM;
+            const totalRebounds = stats.totalOReb + stats.totalDReb;
+            const aveFgPercent = (totalMade / totalAttempts) * 100;
+            const aveThreePtPercent = (stats.totalThreePM / stats.totalThreePA) * 100;
+            const aveFtPercent = (stats.totalFtm / stats.totalFta) * 100;
+            const avePointsPerGame = totalPoints / stats.gamesPlayed;
+            const aveAssistsPerGame = stats.totalAst / stats.gamesPlayed;
+            const aveReboundsPerGame = totalRebounds / stats.gamesPlayed;
+            const aveBlocksPerGame = stats.totalBlk / stats.gamesPlayed;
+            const aveStlPerGame = stats.totalStl / stats.gamesPlayed;
+            const totalLosses = stats.gamesPlayed - stats.totalWins;
+            const winPercent = (stats.totalWins / stats.gamesPlayed) * 100;
+
+            await userInfoRef.doc(updatedUserInfo.id).set({
+                "overviewStats": {
+                    "totalPoints": totalPoints,
+                    "totalAttempts": totalAttempts,
+                    "totalMade": totalMade,
+                    "totalRebounds": totalRebounds,
+                    "aveFgPercent": aveFgPercent,
+                    "aveThreePtPercent": aveThreePtPercent,
+                    "aveFtPercent": aveFtPercent,
+                    "avePointsPerGame": avePointsPerGame,
+                    "aveAssistsPerGame": aveAssistsPerGame,
+                    "aveReboundsPerGame": aveReboundsPerGame,
+                    "aveBlocksPerGame": aveBlocksPerGame,
+                    "aveStlPerGame": aveStlPerGame,
+                    "totalLosses": totalLosses,
+                    "winPercent": winPercent,
+                }
+            }, { merge: true });
+        }
+
+    }
 });
