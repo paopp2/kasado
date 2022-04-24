c434b6c02d64f4dd3eaac534f68f21bec00c486a
Paolo Pepito <npaolopepito@gmail.com>
2022-04-24 20:33:20 +0800

    Implement LocationService

---
+++ lib/data/services/location_service.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+
+import 'package:dartz/dartz.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kasado/model/kasado_location/kasado_location.dart';
+
+final locationServiceProvider =
+    Provider.autoDispose((ref) => LocationService(ref.read));
+
+class LocationService {
+  LocationService(this.read) {
+    // In order to be able to listen to changes for when the location service
+    // is enabled/disabled on top of being able to control the stream, manually
+    // add them through the serviceStatusController which can also be utilized
+    // wherever
+    Geolocator.getServiceStatusStream().listen(
+      (status) => serviceStatusController.add(status),
+    );
+  }
+
+  final Reader read;
+  final serviceStatusController = StreamController<ServiceStatus>();
+  final permissionStatusController =
+      StreamController<LocationPermission>.broadcast();
+
+  Future<Either<Exception, KasadoLocation>> getLocation() async {
+    final serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) Geolocator.openAppSettings();
+    final locPermission = await Geolocator.checkPermission();
+    if (locPermission == LocationPermission.denied) {
+      await Geolocator.requestPermission();
+    }
+    try {
+      final position = await Geolocator.getCurrentPosition();
+
+      return Right(KasadoLocation(
+        lat: position.latitude,
+        lng: position.longitude,
+      ));
+    } catch (e) {
+      e as Exception;
+      switch (e.runtimeType) {
+        case LocationServiceDisabledException:
+          serviceStatusController.add(ServiceStatus.disabled);
+          break;
+        case PermissionDeniedException:
+          permissionStatusController.add(LocationPermission.denied);
+          break;
+      }
+
+      return Left(e);
+    }
+  }
+}
