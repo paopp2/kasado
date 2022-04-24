ef119cf4780810781cf334ef0f9acf350dcb950e
Paolo Pepito <npaolopepito@gmail.com>
2022-04-24 19:44:10 +0800

    WIP: Get courts nearest to a certain location
    'certain location' hardcoded for now

--- lib/data/repositories/court_repository.dart
+++ lib/data/repositories/court_repository.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kasado/data/helpers/firestore_helper.dart';
 import 'package:kasado/data/helpers/firestore_path.dart';
 import 'package:kasado/model/court/court.dart';
 import 'package:kasado/model/kasado_user/kasado_user.dart';
+import 'package:geoflutterfire/geoflutterfire.dart';
 
 final courtRepositoryProvider = Provider.autoDispose(
   (ref) => CourtRepository(
@@ -47,13 +51,59 @@ class CourtRepository {
     );
   }
 
-  Stream<List<Court>> getCourtsStream({KasadoUser? admin}) {
-    return firestoreHelper.collectionStream(
-      path: FirestorePath.colCourts(),
-      builder: (data, docId) => Court.fromJson(data),
-      queryBuilder: (admin != null)
-          ? (query) => query.where('adminIds', arrayContains: admin.id)
-          : null,
-    );
+  // TODO: Remove commented code
+  // Stream<List<Court>> getCourtsStream({KasadoUser? admin}) {
+  //   return firestoreHelper.collectionStream(
+  //     path: FirestorePath.colCourts(),
+  //     builder: (data, docId) => Court.fromJson(data),
+  //     queryBuilder: (admin != null)
+  //         ? (query) => query.where('adminIds', arrayContains: admin.id)
+  //         : null,
+  //   );
+  // }
+
+  Stream<List<Court>> getCourtsStream({
+    KasadoUser? admin,
+    Map<String, double>? centerLatLng,
+  }) {
+    final geo = Geoflutterfire();
+    final streamController = StreamController<List<Court>>();
+    Query<Map<String, dynamic>> query =
+        FirebaseFirestore.instance.collection(FirestorePath.colCourts());
+    if (admin != null) {
+      query = query.where('adminIds', arrayContains: admin.id);
+    }
+    if (centerLatLng != null) {
+      final center = geo.point(
+        latitude: centerLatLng['lat']!,
+        longitude: centerLatLng['lng']!,
+      );
+      final geoRef = geo
+          .collection(collectionRef: query)
+          .within(
+            center: center,
+            radius: 1000,
+            field: 'geo',
+          )
+          .map(
+            (snapList) =>
+                snapList.map((snap) => Court.fromJson(snap.data()!)).toList(),
+          );
+      geoRef.listen((courtList) {
+        streamController.add(courtList);
+      }, cancelOnError: true);
+    } else {
+      final Stream<QuerySnapshot> snapshots = query.snapshots();
+      snapshots.listen((snapshot) {
+        final result = snapshot.docs
+            .map((snapshot) =>
+                Court.fromJson(snapshot.data() as Map<String, dynamic>))
+            .toList();
+        streamController.add(result);
+      }, cancelOnError: true);
+    }
+    streamController.onCancel = streamController.close;
+
+    return streamController.stream;
   }
 }
--- lib/logic/home/states/home_tab_state.dart
+++ lib/logic/home/states/home_tab_state.dart
@@ -2,7 +2,13 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kasado/data/repositories/court_repository.dart';
 
 final courtsStreamProvider = StreamProvider.autoDispose(
-  (ref) => ref.watch(courtRepositoryProvider).getCourtsStream(),
+  (ref) => ref.watch(courtRepositoryProvider).getCourtsStream(
+    // TODO: Replace with real location
+    centerLatLng: {
+      'lat': 10.383033221412328,
+      'lng': 123.97179477071731,
+    },
+  ),
 );
 
 final isLoadedProvider = StateProvider((ref) => false);
