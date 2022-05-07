f2beb4927afa9cfcfc8450ba04d52b3d9a9a4284
Paolo Pepito <npaolopepito@gmail.com>
2022-05-07 11:54:50 +0800

    Add mixpanel event for when navigating to SearchView

--- lib/ui/search/search_view.dart
+++ lib/ui/search/search_view.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kasado/app_router.dart';
+import 'package:kasado/data/core/core_providers.dart';
 import 'package:kasado/ui/shared/user_search/user_search_pane.dart';
 
-class SearchView extends StatelessWidget {
+class SearchView extends HookConsumerWidget {
   const SearchView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    useEffect(() {
+      ref.read(mixpanel)!.track("Navigated to SearchView");
+
+      return;
+    }, []);
+
     return LayoutBuilder(
       builder: (context, constraints) {
         return Scaffold(
