bae10fa306042b9a08ab23783a52ce184862ec1b
Paolo Pepito <npaolopepito@gmail.com>
2022-05-16 17:13:12 +0800

    Wrap UserProfilePane in a Scaffold to have a FAB for editing profile

--- lib/ui/shared/profile_widgets/user_profile_pane.dart
+++ lib/ui/shared/profile_widgets/user_profile_pane.dart
@@ -42,172 +42,183 @@ class UserProfilePane extends HookConsumerWidget {
           ),
         );
 
-    return (user == null)
-        ? const Center(child: Text('No user info'))
-        : Column(
-            children: [
-              const SizedBox(height: 10),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  CircleAvatar(
-                    backgroundImage: NetworkImage(user.photoUrl!),
-                    radius: 40,
-                  ),
-                  SizedBox(
-                    width: constraints.maxWidth * 0.5,
-                    child: Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          AutoSizeText(
-                            user.displayName!,
-                            style: const TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            maxLines: 1,
-                          ),
-                          AutoSizeText(
-                            user.email!,
-                            textAlign: TextAlign.left,
-                            maxLines: 1,
-                          ),
-                          Visibility(
-                            visible: isCurrentUser || isSuperAdmin,
-                            child: GestureDetector(
-                              onTap: () => showDialog(
-                                context: context,
-                                builder: (_) => const PondoInfoDialog(),
+    return Scaffold(
+      body: (user == null)
+          ? const Center(child: Text('No user info'))
+          : Column(
+              children: [
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(user.photoUrl!),
+                      radius: 40,
+                    ),
+                    SizedBox(
+                      width: constraints.maxWidth * 0.5,
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            AutoSizeText(
+                              user.displayName!,
+                              style: const TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
                               ),
-                              onLongPress: (isSuperAdmin)
-                                  ? _onAddPondoLongPressed
-                                  : null,
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  vertical: 8.0,
+                              maxLines: 1,
+                            ),
+                            AutoSizeText(
+                              user.email!,
+                              textAlign: TextAlign.left,
+                              maxLines: 1,
+                            ),
+                            Visibility(
+                              visible: isCurrentUser || isSuperAdmin,
+                              child: GestureDetector(
+                                onTap: () => showDialog(
+                                  context: context,
+                                  builder: (_) => const PondoInfoDialog(),
                                 ),
-                                child: Text(
-                                  '${userInfo?.pondo} Php',
-                                  style: const TextStyle(
-                                    color: Colors.green,
-                                    fontSize: 18,
+                                onLongPress: (isSuperAdmin)
+                                    ? _onAddPondoLongPressed
+                                    : null,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 8.0,
+                                  ),
+                                  child: Text(
+                                    '${userInfo?.pondo} Php',
+                                    style: const TextStyle(
+                                      color: Colors.green,
+                                      fontSize: 18,
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 10),
-              const Divider(),
-              SizedBox(
-                height: constraints.maxHeight * 0.075,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Text(
-                          "5'11",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                        Text(
-                          "Height",
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Text(
-                          "82",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                        Text(
-                          "Weight",
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Text(
-                          "PF / C",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                        Text(
-                          "Position",
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              const Divider(),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                child: TabBar(
-                  labelColor: Colors.black,
-                  unselectedLabelColor: Colors.grey,
-                  controller: tabController,
-                  tabs: const [
-                    Tab(text: "CAREER STATS"),
-                    Tab(text: "GAME HISTORY"),
-                  ],
-                  indicator: MaterialIndicator(),
-                ),
-              ),
-              Expanded(
-                child: (userCareerStats == null)
-                    ? const Center(child: Text('No stats available'))
-                    : Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 15),
-                        child: TabBarView(
-                          controller: tabController,
-                          children: [
-                            CareerStatsListPane(
-                              userStats: userCareerStats,
-                              utils: utils,
-                            ),
-                            GameHistoryListPane(
-                              model: model,
-                              userId: user.id,
-                              constraints: constraints,
-                            ),
                           ],
                         ),
                       ),
-              ),
-            ],
-          );
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                const Divider(),
+                SizedBox(
+                  height: constraints.maxHeight * 0.075,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            "5'11",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Text(
+                            "Height",
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            "82",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Text(
+                            "Weight",
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            "PF / C",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Text(
+                            "Position",
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const Divider(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: TabBar(
+                    labelColor: Colors.black,
+                    unselectedLabelColor: Colors.grey,
+                    controller: tabController,
+                    tabs: const [
+                      Tab(text: "CAREER STATS"),
+                      Tab(text: "GAME HISTORY"),
+                    ],
+                    indicator: MaterialIndicator(),
+                  ),
+                ),
+                Expanded(
+                  child: (userCareerStats == null)
+                      ? const Center(child: Text('No stats available'))
+                      : Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 15),
+                          child: TabBarView(
+                            controller: tabController,
+                            children: [
+                              CareerStatsListPane(
+                                userStats: userCareerStats,
+                                utils: utils,
+                              ),
+                              GameHistoryListPane(
+                                model: model,
+                                userId: user.id,
+                                constraints: constraints,
+                              ),
+                            ],
+                          ),
+                        ),
+                ),
+              ],
+            ),
+      floatingActionButton: isCurrentUser
+          ? FloatingActionButton.extended(
+              label: const Text("Edit Profile"),
+              icon: const Icon(Icons.edit),
+              backgroundColor: Colors.blueGrey,
+              foregroundColor: Colors.white,
+              onPressed: () => print("Allow editing of profile"),
+            )
+          : null,
+    );
   }
 }
